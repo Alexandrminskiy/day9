ca3e90867579fb6c181456b270e9fdd8429d8767
Alexandrminskiy <alexandrminskiy@gmail.com>
2023-01-27 16:48:36 +0300

    day9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd'; //1
+import { useNavigate } from "react-router-dom"; //1
+import { Table, Tag } from 'antd';
+
+const data = [
+  {
+    id: '123',
+    name: 'John Brown',
+  },
+  {
+    id: '1234',
+    key: '2',
+    name: 'Jim Green',
+  },
+  {
+    id: '12345',
+    key: '3',
+    name: 'Joe Black',
+  },
+];
+
 
 function App() {
+  const navigate = useNavigate(); //1
+
+  const handleClick = () => {
+    return navigate('new');
+  }
+
+  const columns = [
+    {
+      title: 'Name',
+      key: 'name',
+      render: (record) => <p>{record.name}</p>,
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (record) => (
+        <div key={record.id} size="middle">
+          <a>Подробнее {record.name}</a>
+        </div>
+      ),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>side Rendering</div>
+      <div>Hello World 1</div>
+      {/* Добавили кнопку переход на другую страницу без обновления 1*/}
+      <Button type="primary" onClick={() => navigate('/new')}>Go new page</Button>
+
+      <Table pagination={false} columns={columns} dataSource={data} />;
     </div>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { New } from './components/New/New';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: < App />,
+    // children: [
+    //   {
+    //     path: 'new',
+    //     element: <New />
+    //   }
+    // ]
+  },
+  {
+    path: "/new",
+    element: <New />,
+  },
+  {
+    path: "/users",
+    element: <New />,
+  },
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
